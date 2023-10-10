724c515e382537d2d4395ec15eb793b1c83dcbcb
Dhouchl <34416808+pangchewe@users.noreply.github.com>
2023-10-11 04:55:14 +0700

    Add files via upload

---
+++ src/components/Tables/OrderedTable.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <md-table v-model="users" :table-header-color="tableHeaderColor">
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="ID">{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Salary">{{ item.salary }}</md-table-cell>
+        <md-table-cell md-label="Country">{{ item.country }}</md-table-cell>
+        <md-table-cell md-label="City">{{ item.city }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ordered-table",
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      selected: [],
+      users: [
+        {
+          id: 1,
+          name: "ธัชธนินท์ ลีเชี่ยวยุวสรณ์",
+          salary: "$99,999",
+          country: "TH",
+          city: "Nakhon Pathom",
+        },
+        {
+          id: 2,
+          name: "โนบิ โนบิตะ",
+          salary: "$23,738",
+          country: "TH",
+          city: "Bangkok",
+        },
+        {
+          id: 3,
+          name: "Sage Rodriguez",
+          salary: "$56,142",
+          country: "Netherlands",
+          city: "Overland Park",
+        },
+        {
+          id: 4,
+          name: "Philip Chaney",
+          salary: "$38,735",
+          country: "Korea, South",
+          city: "Gloucester",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/pages/Layout/ContentFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/dhouch/">AIRS</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/dhouch/"> About Us </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/dhouch/"> Plugins </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/dhouch/"> Licenses </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        &copy; {{ new Date().getFullYear() }}
+        <a href="https://www.linkedin.com/in/dhouch/" target="_blank"
+          >Pang</a
+        >, made with <i class="fa fa-heart heart"></i> for a better retail
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
